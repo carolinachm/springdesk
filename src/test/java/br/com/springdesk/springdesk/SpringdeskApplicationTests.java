7726a87be6460458ce39e06fcc0458a9b4ce084c
Carolina <carolti2013@gmail.com>
2023-08-11 15:55:48 -0300

    criação da arquitetura do projeto spring desk

---
+++ src/test/java/br/com/springdesk/springdesk/SpringdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springdesk.springdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
